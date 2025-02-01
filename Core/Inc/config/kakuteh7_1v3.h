af2640454ed2132de9ab7bc479032f1b74a4f4fe
Cenk Keskin <cnkkskn001@gmail.com>
2025-02-01 20:49:35 +0300

    Volt calibration macro is defined in their config files

--- Core/Inc/config/kakuteh7_1v3.h
+++ Core/Inc/config/kakuteh7_1v3.h
@@ -1,4 +1,6 @@
 
+#define VOLT_CALIBRATION_MULTIPLIER		11.1f
+
 #define TARGET_MAIN_TIME_US			TARGET_8KHZ_US
 #define TARGET_GYRO_TIME_US			TARGET_8KHZ_US
 #define TARGET_ACC_TIME_US			TARGET_8KHZ_US
@@ -14,45 +16,43 @@
 	#define MAIN_INTERRUPT_Handler		TIM16_IRQHandler
 	#define MAIN_INTERRUPT_IRQn			TIM16_IRQn
 
- // My board had 2 ICM42688P i changed it
- // SPI1 is IIM42652
- // SPI4 is ICM42688P
 #if GYRO1_SPI_
-	#define GYRO1_SPI           		SPI1 // Checked on hardware
-	#define GYRO1_CS_PORT				GPIOC // Checked on hardware
-	#define GYRO1_CS_PIN				15
-	#define TARGET_GYRO1				IIM42652_GYRO
+	#define GYRO1_SPI           		SPI4
+	#define GYRO1_CS_PORT				GPIOE
+	#define GYRO1_CS_PIN				4
+	#define TARGET_GYRO1				MPU6000_GYRO
 
 	#define GYRO1_USE_INT				0
-	#define GYRO1_INT_PORT				GPIOB
-	#define GYRO1_INT_PIN				2
+	#define GYRO1_INT_PORT				GPIOE
+	#define GYRO1_INT_PIN				1
 
 	#define USE_DMA_SENSOR				1
 	#define USE_DMA_SENSOR_ICM42688P	0
 	#define USE_DMA_SENSOR_ICM20602		0
-	#define USE_DMA_SENSOR_IIM42652		1
+	#define USE_DMA_SENSOR_IIM42652		0
+	#define USE_DMA_SENSOR_MPU6000		1
 	#define SENSOR_DMA					DMA1
 	#define SENSOR_DMA_TX_Stream		DMA1_Stream6
 	#define SENSOR_DMA_RX_Stream		DMA1_Stream7
 	#define SENSOR_DMA_TX_Handler		DMA1_Stream6_IRQHandler
 	#define SENSOR_DMA_RX_Handler		DMA1_Stream7_IRQHandler
-	#define SENSOR_DMA_Request1			DMA_REQUEST_SPI1_TX
-	#define SENSOR_DMA_Request2			DMA_REQUEST_SPI1_RX
+	#define SENSOR_DMA_Request1			DMA_REQUEST_SPI4_TX
+	#define SENSOR_DMA_Request2			DMA_REQUEST_SPI4_RX
 
 	#define SENSOR_DMA_TX_IRQn			DMA1_Stream6_IRQn
 	#define SENSOR_DMA_RX_IRQn			DMA1_Stream7_IRQn
 
-	#define GYRO1_SPI_CLOCK				16000000L // ICM42688P 24MHz but works with 32MHz
+	#define GYRO1_SPI_CLOCK				16000000L
 
 #endif
 
 #if ACC1_SPI_
-	#define ACC1_SPI           			SPI1
-	#define ACC1_CS_PORT				GPIOC
-	#define ACC1_CS_PIN					15
-	#define TARGET_ACC1					IIM42652_ACC
+	#define ACC1_SPI           			SPI4
+	#define ACC1_CS_PORT				GPIOE
+	#define ACC1_CS_PIN					4
+	#define TARGET_ACC1					MPU6000_ACC
 
-	#define ACC1_SPI_CLOCK				16000000L // ICM42688P 24MHz but works with 32MHz
+	#define ACC1_SPI_CLOCK				16000000L
 #endif
 
 #if GYRO2_SPI_
@@ -133,6 +133,12 @@
 	#define BARO_CS_PIN        			4
 
 	#define BARO_SPI_CLOCK				10000000L
+	#define TARGET_BARO					BMP280_BAROMETER
+#endif
+
+#if BARO_I2C_
+	#define BARO_I2C          			I2C1
+	#define TARGET_BARO					BMP280_BAROMETER
 #endif
 
 #if MAG_SPI_
@@ -141,10 +147,12 @@
 	#define MAG_CS_PIN					1
 
 	#define MAG_SPI_CLOCK				10000000L
+	#define TARGET_MAG					QMC5883L_MAGNETO
 #endif
 
 #if MAG_I2C_
 	#define MAG_I2C           			I2C1
+	#define TARGET_MAG					QMC5883L_MAGNETO
 #endif
 
 #if BNO055_
@@ -153,13 +161,13 @@
 
 // GPS
 #if GPS_
-	#define GPS_UART					USART1
+	#define GPS_UART					UART4
 	#define USE_INTERRUPT_GPS			1
-	#define GPS_INTERRUPT_				1
+	#define GPS_INTERRUPT_				4
 #endif
 
-	#define BUZZER_GPIO					GPIOA // BUZ- pin is DC active low but i use pwm to set volume
-	#define BUZZER_GPIO_PIN				15
+	#define BUZZER_GPIO					GPIOC // BUZ- pin is DC active low but i use pwm to set volume
+	#define BUZZER_GPIO_PIN				13
 
 #if BUZZER_PWM
 	#define BUZZER_TIM					TIM2
@@ -177,12 +185,15 @@
 
 	#define MICROCARD_DMA           	DMA2
 	#define MICROCARD_DMA_STREAM    	DMA2_Stream0
-	#define MICROCARD_SPI           	SPI3
+	#define MICROCARD_SPI           	SPI1
 	#define MICROCARD_CS_PORT       	GPIOA
-	#define MICROCARD_CS_PIN        	8
+	#define MICROCARD_CS_PIN        	4
+
+	#define MICROCARD_DETECT_PORT		GPIOA
+	#define MICROCARD_DETECT_PIN		3
 
 	#define MICROCARD_DMA_TX_Handler	DMA2_Stream0_IRQHandler
-	#define MICROCARD_DMA_Request1		DMA_REQUEST_SPI3_TX
+	#define MICROCARD_DMA_Request1		DMA_REQUEST_SPI1_TX
 
 	#define MICROCARD_DMA_TX_IRQn		DMA2_Stream0_IRQn
 
@@ -206,13 +217,13 @@
 	// DSHOT and PWM PROTOCOL
 	#define MOTOR1_TIM					TIM3
 	#define MOTOR2_TIM					TIM3
-	#define MOTOR3_TIM					TIM5
-	#define MOTOR4_TIM					TIM5
+	#define MOTOR3_TIM					TIM2
+	#define MOTOR4_TIM					TIM2
 
 	#define MOTOR1_TIM_CH				TIM_CHANNEL_3
 	#define MOTOR2_TIM_CH				TIM_CHANNEL_4
-	#define MOTOR3_TIM_CH				TIM_CHANNEL_1
-	#define MOTOR4_TIM_CH				TIM_CHANNEL_2
+	#define MOTOR3_TIM_CH				TIM_CHANNEL_2
+	#define MOTOR4_TIM_CH				TIM_CHANNEL_3
 
 	#define MOTOR1_GPIO					GPIOB
 	#define MOTOR1_PIN					0
@@ -222,13 +233,13 @@
 	#define MOTOR2_PIN					1
 	#define MOTOR2_AF					CK_GPIO_AF2
 
-	#define MOTOR3_GPIO					GPIOA
-	#define MOTOR3_PIN					0
-	#define MOTOR3_AF					CK_GPIO_AF2
+	#define MOTOR3_GPIO					GPIOB
+	#define MOTOR3_PIN					3
+	#define MOTOR3_AF					CK_GPIO_AF1
 
-	#define MOTOR4_GPIO					GPIOA
-	#define MOTOR4_PIN					1
-	#define MOTOR4_AF					CK_GPIO_AF2
+	#define MOTOR4_GPIO					GPIOB
+	#define MOTOR4_PIN					10
+	#define MOTOR4_AF					CK_GPIO_AF1
 
 	#define DSHOT1_DMA					DMA1
 	#define DSHOT1_DMA_Stream			DMA1_Stream0
@@ -246,15 +257,15 @@
 
 	#define DSHOT3_DMA					DMA1
 	#define DSHOT3_DMA_Stream			DMA1_Stream2
-	#define DSHOT3_DMA_Request			DMA_REQUEST_TIM5_CH1
-	#define DSHOT3_DMA_ID				TIM_DMA_ID_CC1
+	#define DSHOT3_DMA_Request			DMA_REQUEST_TIM2_CH2
+	#define DSHOT3_DMA_ID				TIM_DMA_ID_CC2
 	#define DSHOT3_DMA_Handler			DMA1_Stream2_IRQHandler
 	#define DSHOT3_DMA_IRQn				DMA1_Stream2_IRQn
 
 	#define DSHOT4_DMA					DMA1
 	#define DSHOT4_DMA_Stream			DMA1_Stream3
-	#define DSHOT4_DMA_Request			DMA_REQUEST_TIM5_CH2
-	#define DSHOT4_DMA_ID				TIM_DMA_ID_CC2
+	#define DSHOT4_DMA_Request			DMA_REQUEST_TIM2_CH3
+	#define DSHOT4_DMA_ID				TIM_DMA_ID_CC3
 	#define DSHOT4_DMA_Handler			DMA1_Stream3_IRQHandler
 	#define DSHOT4_DMA_IRQn				DMA1_Stream3_IRQn
 
@@ -265,25 +276,25 @@
 
 
 #if RGB_
-	#define RGB_TIM						TIM1
+	#define RGB_TIM						TIM4
 
 	#define RGB_TIM_CH					TIM_CHANNEL_1
 
-	#define RGB_GPIO					GPIOA
-	#define RGB_PIN						8
-	#define RGB_AF						CK_GPIO_AF1
+	#define RGB_GPIO					GPIOD
+	#define RGB_PIN						12
+	#define RGB_AF						CK_GPIO_AF2
 
 	#define RGB_DMA						DMA1
 	#define RGB_DMA_Stream				DMA1_Stream4
-	#define RGB_DMA_Request				DMA_REQUEST_TIM1_CH1
+	#define RGB_DMA_Request				DMA_REQUEST_TIM4_CH1
 	#define RGB_DMA_ID					TIM_DMA_ID_CC1
 	#define RGB_DMA_Handler				DMA1_Stream4_IRQHandler
 	#define RGB_DMA_IRQn				DMA1_Stream4_IRQn
 #endif
 
 #if LED1_
-	#define LED1_GPIO					GPIOE
-	#define LED1_GPIO_PIN				3
+	#define LED1_GPIO					GPIOC
+	#define LED1_GPIO_PIN				2
 	#define LED1_ACTIVE_LOW				1
 #endif
 
@@ -305,11 +316,11 @@
 	#define OSD_CS_PIN					12
 
 	#define USE_DMA_MAX7456				1
-	#define OSD_MAX7456_DMA						DMA1
-	#define OSD_MAX7456_DMA_Stream				DMA1_Stream5
-	#define OSD_MAX7456_DMA_Handler				DMA1_Stream5_IRQHandler
-	#define OSD_MAX7456_DMA_Request				DMA_REQUEST_SPI2_TX
-	#define OSD_MAX7456_DMA_IRQn				DMA1_Stream5_IRQn
+	#define OSD_MAX7456_DMA				DMA1
+	#define OSD_MAX7456_DMA_Stream		DMA1_Stream5
+	#define OSD_MAX7456_DMA_Handler		DMA1_Stream5_IRQHandler
+	#define OSD_MAX7456_DMA_Request		DMA_REQUEST_SPI2_TX
+	#define OSD_MAX7456_DMA_IRQn		DMA1_Stream5_IRQn
 
 	#define OSD_SPI_CLOCK				16000000L
 
@@ -330,17 +341,17 @@
 #endif
 
 #if OSD_DJI_
-	#define OSD_DJI_USART				USART2
+	#define OSD_DJI_USART				USART1
 
 	#define USE_INTERRUPT_OSD			1
-	#define OSD_INTERRUPT_				2
+	#define OSD_INTERRUPT_				1
 
 	#define USE_DMA_OSD					0
 	#define OSD_DMA						DMA2
 	#define OSD_DMA_Stream				DMA2_Stream2
 	#define OSD_DMA_Handler				DMA2_Stream2_IRQHandler
 	#define OSD_DMA_IRQn				DMA2_Stream2_IRQn
-	#define OSD_DMA_Request				DMA_REQUEST_USART2_TX
+	#define OSD_DMA_Request				DMA_REQUEST_USART1_TX
 #endif
 
 #if SBUS_
@@ -382,8 +393,8 @@
 #endif
 
 #if CAMERA_SWITCH_
-	#define CAMERA_SWITCH_GPIO			GPIOD
-	#define CAMERA_SWITCH_GPIO_PIN		11
+	#define CAMERA_SWITCH_GPIO			GPIE
+	#define CAMERA_SWITCH_GPIO_PIN		9
 	#define CAMERA_SELECT				1
 #endif
 
