67371e84e6955c865179210bd665af816857fea6
Cenk Keskin <cnkkskn001@gmail.com>
2025-03-09 15:49:40 +0300

    changes

--- Core/Inc/FLIGHT/CK_LOG.c
+++ Core/Inc/FLIGHT/CK_LOG.c
@@ -23,8 +23,8 @@
 #define LOG_TIMEOUT_MS 				240
 #define SDCARD_CHECK_BUSY_US		TARGET_PERIPHERAL_TIME_US * 5
 
-const uint8_t debug_start_byte = 0x33;
-const uint8_t debug_end_byte   = 0x55;
+const uint8_t debug_start_byte = 0x30;
+const uint8_t debug_end_byte   = 0x50;
 uint8_t counter = debug_start_byte;
 
 log_parameters_t flightLog;
