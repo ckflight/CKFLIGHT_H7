65606f61ba489911fe7a220ce9212ebf78bdc0d5
ckflight <cnkkskn001@gmail.com>
2025-03-30 22:10:50 +0300

    Settings change

--- Core/Inc/CK_SETTINGS.h
+++ Core/Inc/CK_SETTINGS.h
@@ -33,7 +33,7 @@
 #define LOG_FLASH_				0
 #define LOG_DUALBUFFER_			0
 #define LOG_MULTIWRITE_			0// spi works in single mode
-#define TEST_LOG				1
+#define TEST_LOG				0
 
 #define BUZZER_PWM				0
 #define BUZZER_DC				1
